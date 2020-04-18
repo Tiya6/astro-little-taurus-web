b2bff61392e4ca037f35beed5c17ea8c6b8ff893
Tiya6 <tiyaseiss@gmail.com>
2020-04-18 12:06:39 +0200

    modified navbar, to show depending on user logged in or not

--- src/components/misc/navbar/Navbar.js
+++ src/components/misc/navbar/Navbar.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import './Navbar.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import Logout from '../../users/Logout';
+import { Button, Nav } from 'react-bootstrap'
+import { WithAuthConsumer } from '../../../contexts/AuthContext';
 
-const navbar = () => {
-  return (
+class navbar extends Component {
+
+    render() {
+        let login
+        let profile
+        let register
+        let redirect
+        if(this.props.currentUser) {
+            profile = <Link to="/user" className="link" >PROFILE</Link>
+            login = <Logout/>            
+        } else {
+            login = <Link to="/login" className="link" variant="primary">LOGIN</Link>
+            register = <Link to="/register" className="link" variant="primary">SIGN UP</Link>
+            redirect = <Redirect to="/"/>
+        }
+        return (
         <nav className="navbar navbar-dark bg navbar-inverse">
             <ul className="nav navbar-nav list-group list-group-horizontal-xl">
                 <li><Link to='/learn' className="link">LEARN</Link></li>
@@ -15,12 +32,13 @@ const navbar = () => {
                 <li><Link to='/' className='link'>ASTRO LITTLE TAURUS</Link></li>
             </ul>
             <ul className="navbar navbar-nav list-group list-group-horizontal-xl navbar-right">
-                <li><Link to='/login' className="link">LOGIN</Link></li>
-                <li><Link to='/register' className="link">REGISTER</Link></li>
-                <li><Link to='/logout' className="link">LOGOUT</Link></li>            
+                <li className="link">{profile}</li>
+                <li className="link">{login}</li>
+                <li className="link">{register}</li>  
+                <li className="link">{redirect}</li>    
             </ul>
         </nav>
   )
 }
-
-export default navbar;
+}
+export default WithAuthConsumer(navbar) ;
